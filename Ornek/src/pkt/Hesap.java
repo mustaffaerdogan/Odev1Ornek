a31561327f975be424b33cd24e99889d22d08f0c
Mustafa Erdoğan <49840036+mustaffaerdogan@users.noreply.github.com>
2024-04-07 17:24:33 +0300

    Update Hesap.java

--- Ornek/src/pkt/Hesap.java
+++ Ornek/src/pkt/Hesap.java
@@ -7,11 +7,11 @@ import java.util.UUID;
  * @author MFA
  *
  */
-public class Hesap implements IHesap {
+public class Hesap {
 	private String no;
 	private double bakiye;
 	
-	public Hesab()
+	public Hesap()
 	{
 		this.no = UUID.randomUUID().toString(); // Rastgele bir Id belirleniyor...
 		// Hesap açılışı bakiye sıfır.
